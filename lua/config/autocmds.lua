b9182140544bb8776242259dc30ff217f42afc96
i18n <i18n.site@gmail.com>
2025-07-05 09:21:39 +0800

    ♨

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,21 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+  group = vim.api.nvim_create_augroup("MyTemplateLoader", { clear = true }),
+  pattern = "*.*",
+  callback = function(args)
+    local extension = vim.fn.fnamemodify(args.file, ":e")
+    if extension == "" then
+      return
+    end
+
+    local template_path = vim.fn.stdpath("config") .. "/bundle/template/vim." .. extension
+    if vim.fn.filereadable(template_path) == 1 then
+      local template_content = vim.fn.readfile(template_path)
+      vim.api.nvim_buf_set_lines(args.buf, 0, -1, false, template_content)
+    end
+  end,
+})
+
