f767fe361f36fcde753f570833d33219249c4c91
i18n <i18n.site@gmail.com>
2025-07-08 14:48:04 +0800

    ♨

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,18 @@
-local set = vim.keymap.set
-set("v", "<BS>", "gc", { remap = true, desc = "comment selection" })
-set("v", "<", "<gv", { noremap = true, silent = true })
-set("v", ">", ">gv", { noremap = true, silent = true })
-set("v", ".", ":<C-u>normal .<CR>", { desc = "在可视模式下重复上一个操作" })
+-- fix https://github.com/LazyVim/LazyVim/discussions/6235
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  group = vim.api.nvim_create_augroup("MyKeymaps", { clear = true }),
+  pattern = "*",
+  callback = function()
+    local set = vim.keymap.set
+    set("v", "<", "<gv", { noremap = true, silent = true, buffer = true })
+    set("v", ">", ">gv", { noremap = true, silent = true, buffer = true })
+    set(
+      "v",
+      ".",
+      ":<C-u>normal .<CR>",
+      { silent = true, desc = "在可视模式下重复上一个操作", buffer = true }
+    )
+    set("v", "<BS>", "gc", { remap = true, desc = "comment selection", buffer = true })
+  end,
+})
