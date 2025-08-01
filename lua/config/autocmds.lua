0fb0538530989be95f54f4d30433379a73aeed49
i18n <i18n.site@gmail.com>
2025-08-01 11:19:45 +0800

    .

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,15 +1,6 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-
 -- 文件的后缀模板
 vim.api.nvim_create_autocmd("BufNewFile", {
-  group = vim.api.nvim_create_augroup("MyTemplateLoader", { clear = true }),
+  group = vim.api.nvim_create_augroup("TemplateLoader", { clear = true }),
   pattern = "*.*",
   callback = function(args)
     local extension = vim.fn.fnamemodify(args.file, ":e")
