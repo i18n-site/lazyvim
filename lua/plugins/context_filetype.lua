a5df812d3e63fac976725d8df761731b25053ab5
i18n <i18n.site@gmail.com>
2025-08-01 18:23:03 +0800

    ♨

--- lua/plugins/context_filetype.lua
+++ lua/plugins/context_filetype.lua
@@ -1,13 +1,5 @@
 return {
-  "Shougo/context_filetype.vim",
+  "i18n-fork/context_filetype.vim",
   ft = "svelte",
-  init = function()
-    vim.g.context_filetype_filetypes = {
-      svelte = {
-        { filetype = "coffee", start = '<script lang="coffee">', ["end"] = "</script>" },
-        { filetype = "stylus", start = '<style lang="stylus">', ["end"] = "</style>" },
-        { filetype = "pug", start = '<template lang="pug">', ["end"] = "</template>" },
-      },
-    }
-  end,
+  branch = "master",
 }
